40196ee6b892e37b13848c6b9a8eb8236608e4a6
Shivang Raina <shivangraina70@gmail.com>
2021-02-27 00:50:04 +0530

    "Linted and added comments"

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"net/http"
 )
+//BaseURL ..
+var BaseURL string = "https://codeforces.com/api" 
 
-var BaseUrl string = "https://codeforces.com/api"
-
+//Results ..
 type Results struct {
 	Result []Result `json:"result"`
 }
-
+//Result ..
 type Result struct {
 	ID              int64   `json:"id"`
 	ContestID       int64   `json:"contestId"`
@@ -20,12 +21,14 @@ type Result struct {
 	Verdict         string  `json:"verdict"`
 	Author          Author  `json:"author"`
 }
+// Problem ..
 type Problem struct {
 	Index  string   `json:"index"`
 	Name   string   `json:"name"`
 	Tags   []string `json:"tags"`
 	Rating int64    `json:"rating"`
 }
+//Author ..
 type Author struct {
 	ParticipantType string `json:"participantType"`
 }
@@ -35,27 +38,30 @@ func main() {
 	fmt.Println(PlayedContests())
 
 }
+//FetchContestData ..
 func FetchContestData(jobs <-chan int64, results chan<- Results) {
 	var Handle string = "RemoteCodeExecution" //take from cmd
 	var wrapper Results
 
 	for contestid := range jobs {
 
-		url := fmt.Sprintf(BaseUrl+"/contest.status?contestId=%d&handle=%s&from=1&count=50", contestid, Handle)
+		url := fmt.Sprintf(BaseURL+"/contest.status?contestId=%d&handle=%s&from=1&count=50", contestid, Handle)
 		fmt.Println(url)
 
-		FetchJson(url, &wrapper)
+		FetchJSON(url, &wrapper)
 		results <- wrapper
 
 	}
 
 }
+//GetAllContests ..
 func GetAllContests() []int64 {
 	var wrapper Results
 	var contestids []int64
-	url := fmt.Sprintf(BaseUrl + "/contest.list")
+	url := fmt.Sprintf(BaseURL + "/contest.list")
+	
 
-	FetchJson(url, &wrapper)
+	FetchJSON(url, &wrapper)
 	for _, result := range wrapper.Result {
 		contestids = append(contestids, result.ID)
 
@@ -63,6 +69,7 @@ func GetAllContests() []int64 {
 	return contestids
 
 }
+//PlayedContests ..
 func PlayedContests() []int64 {
 
 	contestids := GetAllContests()
@@ -92,7 +99,8 @@ func PlayedContests() []int64 {
 	}
 	return played
 }
-func FetchJson(url string, wrapper interface{}) error {
+//FetchJSON ..
+func FetchJSON(url string, wrapper interface{}) error {
 	res, err := http.Get(url)
 	if err != nil {
         fmt.Println(err)
